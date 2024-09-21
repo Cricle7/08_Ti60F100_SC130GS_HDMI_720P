99e1274f47e7a4202f9953d78413daa2ef9f030d
circle7 <2363720718@qq.com>
2024-09-21 19:13:44 +0800

    use interconnector.v

--- example_top.v
+++ example_top.v
@@ -667,131 +667,6 @@ module example_top
 	wire 			w_hbram_cal_done = w_hbram_cal_pass; 
 	wire 	[15:0] 	w_hbram_cal_dbg; 
 	
-	assign w_dev_rdata_i = {w_hbram_cal_dbg, 15'h0, w_hbram_cal_pass}; 
-	
-	hbram u_hbram (		
-		.ram_clk			(hbramClk),			//	input ram_clk,
-		.ram_clk_cal		(hbramClk_Cal),		//	input ram_clk_cal,
-		.io_axi_clk			(sys_clk_i),		//	input io_axi_clk,
-		.rst				(w_hbram_ui_areset),	//	input rst,
-		
-		.hbc_cal_SHIFT_SEL	(hbramClk_shift_sel),	//	output [4:0] hbc_cal_SHIFT_SEL,
-		.hbc_cal_SHIFT		(hbramClk_shift),		//	output [2:0] hbc_cal_SHIFT,
-		.hbc_cal_SHIFT_ENA	(hbramClk_shift_ena),	//	output hbc_cal_SHIFT_ENA,
-		.hbc_cal_debug_info	(w_hbram_cal_dbg),		//	output [15:0] hbc_cal_debug_info,
-		.hbc_cal_pass		(w_hbram_cal_pass),	//	output hbc_cal_pass,
-		
-		.dyn_pll_phase_sel	(0),				//	input [2:0] dyn_pll_phase_sel,
-		.dyn_pll_phase_en		(0),				//	input dyn_pll_phase_en,
-		
-		.io_arw_payload_addr	(w_hbram_aaddr),		//	input [31:0] io_arw_payload_addr,
-		.io_arw_payload_id	(w_hbram_aid),		//	input [7:0] io_arw_payload_id,
-		.io_arw_payload_len	(w_hbram_alen),		//	input [7:0] io_arw_payload_len,
-		.io_arw_payload_size	(w_hbram_asize),		//	input [2:0] io_arw_payload_size,
-		.io_arw_payload_burst	(w_hbram_aburst),		//	input [1:0] io_arw_payload_burst,
-		.io_arw_payload_lock	(w_hbram_alock),		//	input [1:0] io_arw_payload_lock,
-		.io_arw_payload_write	(w_hbram_atype),		//	input io_arw_payload_write, 		//	0:Read. 1:Write
-		.io_arw_valid		(w_hbram_avalid),		//	input io_arw_valid,
-		.io_arw_ready		(w_hbram_aready),		//	output io_arw_ready,
-			
-		.io_w_payload_id		(w_hbram_wid),		//	input [7:0] io_w_payload_id,
-		.io_w_payload_data	(w_hbram_wdata),		//	input [127:0] io_w_payload_data,
-		.io_w_payload_strb	(w_hbram_wstrb),		//	input [15:0] io_w_payload_strb,
-		.io_w_payload_last 	(w_hbram_wlast),		//	input io_w_payload_last,	
-		.io_w_valid			(w_hbram_wvalid),		//	input io_w_valid,
-		.io_w_ready			(w_hbram_wready),		//	output io_w_ready,
-		
-		.io_b_payload_id		(w_hbram_bid),		//	output [7:0] io_b_payload_id,
-		.io_b_valid			(w_hbram_bvalid),		//	output io_b_valid,
-		.io_b_ready			(w_hbram_bready),		//	input io_b_ready,
-
-		.io_r_payload_id		(w_hbram_rid),		//	output [7:0] io_r_payload_id,
-		.io_r_payload_data	(w_hbram_rdata),		//	output [127:0] io_r_payload_data,
-		.io_r_payload_last	(w_hbram_rlast),		//	output io_r_payload_last,
-		.io_r_payload_resp	(w_hbram_rresp),		//	output [1:0] io_r_payload_resp
-		.io_r_valid			(w_hbram_rvalid),		//	output io_r_valid,
-		.io_r_ready			(w_hbram_rready),		//	input io_r_ready,
-		
-		.hbc_ck_p_HI		(hbram_CK_P_HI),		//	output hbc_ck_p_HI,
-		.hbc_ck_p_LO		(hbram_CK_P_LO),		//	output hbc_ck_p_LO,
-		.hbc_ck_n_HI		(hbram_CK_N_HI),		//	output hbc_ck_n_HI,
-		.hbc_ck_n_LO		(hbram_CK_N_LO),		//	output hbc_ck_n_LO,
-		.hbc_cs_n			(hbram_CS_N),		//	output hbc_cs_n,
-		.hbc_rst_n			(hbram_RST_N),		//	output hbc_rst_n,
-
-		.hbc_dq_OUT_HI		(hbram_DQ_OUT_HI),	//	output [15:0] hbc_dq_OUT_HI,
-		.hbc_dq_OUT_LO		(hbram_DQ_OUT_LO),	//	output [15:0] hbc_dq_OUT_LO,
-		.hbc_dq_OE			(hbram_DQ_OE),		//	output [15:0] hbc_dq_OE,
-		.hbc_dq_IN_HI		(hbram_DQ_IN_HI),		//	input [15:0] hbc_dq_IN_HI,
-		.hbc_dq_IN_LO		(hbram_DQ_IN_LO),		//	input [15:0] hbc_dq_IN_LO,
-		
-		.hbc_rwds_OUT_HI		(hbram_RWDS_OUT_HI),	//	output [1:0] hbc_rwds_OUT_HI,
-		.hbc_rwds_OUT_LO		(hbram_RWDS_OUT_LO),	//	output [1:0] hbc_rwds_OUT_LO,
-		.hbc_rwds_OE		(hbram_RWDS_OE),		//	output [1:0] hbc_rwds_OE,
-		.hbc_rwds_IN_HI		(hbram_RWDS_IN_HI),	//	input [1:0] hbc_rwds_IN_HI,
-		.hbc_rwds_IN_LO		(hbram_RWDS_IN_LO)	//	input [1:0] hbc_rwds_IN_LO,
-	);
-	assign w_hbram_bready = 1'b1; 
-	
-	`ifdef SIMULATION
-		AXI4_AWARMux #(.AID_LEN(4), .AADDR_LEN(32)) axi4_awar_mux (
-			.aclk_i			(sys_clk_i), 
-			.arst_i			(w_hbram_ui_rst), 
-		
-			.awid_i			(w_hbram_awid),
-			.awaddr_i			(w_hbram_awaddr),
-			.awlen_i			(w_hbram_awlen),
-			//.awvalid_i			(w_hbram_awvalid && w_hbram_cal_pass),
-			.awvalid_i			(w_hbram_awvalid ),
-			.awready_o			(w_hbram_awready),
-		
-			.arid_i			(w_hbram_arid),
-			.araddr_i			(w_hbram_araddr),
-			.arlen_i			(w_hbram_arlen),
-			//.arvalid_i			(w_hbram_arvalid && w_hbram_cal_pass),
-			.arvalid_i			(w_hbram_arvalid ),
-			.arready_o			(w_hbram_arready),
-		
-			.aid_o			(w_hbram_aid),
-			.aaddr_o			(w_hbram_aaddr),
-			.alen_o			(w_hbram_alen),
-			.atype_o			(w_hbram_atype),
-			.avalid_o			(w_hbram_avalid),
-			.aready_i			(w_hbram_aready)
-		);
-
-	`else			
-		AXI4_AWARMux #(.AID_LEN(4), .AADDR_LEN(32)) axi4_awar_mux (
-			.aclk_i			(sys_clk_i), 
-			.arst_i			(w_hbram_ui_rst), 
-		
-			.awid_i			(w_hbram_awid),
-			.awaddr_i			(w_hbram_awaddr),
-			.awlen_i			(w_hbram_awlen),
-			//.awvalid_i			(w_hbram_awvalid && w_hbram_cal_pass),
-			.awvalid_i			(w_hbram_awvalid ),
-			.awready_o			(w_hbram_awready),
-		
-			.arid_i			(w_hbram_arid),
-			.araddr_i			(w_hbram_araddr),
-			.arlen_i			(w_hbram_arlen),
-			//.arvalid_i			(w_hbram_arvalid && w_hbram_cal_pass),
-			.arvalid_i			(w_hbram_arvalid ),
-			.arready_o			(w_hbram_arready),
-		
-			.aid_o			(w_hbram_aid),
-			.aaddr_o			(w_hbram_aaddr),
-			.alen_o			(w_hbram_alen),
-			.atype_o			(w_hbram_atype),
-			.avalid_o			(w_hbram_avalid),
-			.aready_i			(w_hbram_aready)
-		);
-	`endif	
-
-	assign w_hbram_asize = 4; 		//	Fixed 128 bits (16 bytes, size = 4)
-	assign w_hbram_aburst = 1; 
-	assign w_hbram_alock = 0; 
-	
 	
 	
 
@@ -1083,16 +958,6 @@ module example_top
 
 
 
-	
-	
-
-
-
-
-
-
-
-
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//	DDR R/W Control
@@ -1108,57 +973,45 @@ module example_top
 	wire            [15:0]          lcd_data;
 	
 	wire 	[7:0] 	w_axi_tp; 
-	axi4_ctrl #(.C_RD_END_ADDR(1280 * 720), .C_W_WIDTH(CSI_DATA_WIDTH), .C_R_WIDTH(8), .C_ID_LEN(4)) u_axi4_ctrl (
-	//axi4_ctrl #(.C_RD_END_ADDR(1920 * 1080), .C_W_WIDTH(CSI_DATA_WIDTH), .C_R_WIDTH(8), .C_ID_LEN(4)) u_axi4_ctrl (
 
-		.axi_clk        (sys_clk_i       ),
-		.axi_reset      (w_hbram_ui_rst       ),
-
-		.axi_awaddr     (w_hbram_awaddr       ),
-		.axi_awlen      (w_hbram_awlen        ),
-		.axi_awvalid    (w_hbram_awvalid      ),
-		.axi_awready    (w_hbram_awready      ),
-
-		.axi_wdata      (w_hbram_wdata        ),
-		.axi_wstrb      (w_hbram_wstrb        ),
-		.axi_wlast      (w_hbram_wlast        ),
-		.axi_wvalid     (w_hbram_wvalid       ),
-		.axi_wready     (w_hbram_wready       ),
-
-		.axi_bid        (0          ),
-		.axi_bresp      (0        ),
-		.axi_bvalid     (1       ),
-
-		.axi_arid       (w_hbram_arid         ),
-		.axi_araddr     (w_hbram_araddr       ),
-		.axi_arlen      (w_hbram_arlen        ),
-		.axi_arvalid    (w_hbram_arvalid      ),
-		.axi_arready    (w_hbram_arready      ),
-
-		.axi_rid        (w_hbram_rid          ),
-		.axi_rdata      (w_hbram_rdata        ),
-		.axi_rresp      (0        ),
-		.axi_rlast      (w_hbram_rlast        ),
-		.axi_rvalid     (w_hbram_rvalid       ),
-		.axi_rready     (w_hbram_rready       ),
-
-		.wframe_pclk    (w_csi_rx_clk          ),
-		.wframe_vsync   (cmos_frame_vsync), 	//w_wframe_vsync   ),		//	Writter VSync. Flush on rising edge. Connect to EOF. 
-		.wframe_data_en (cmos_frame_href   ),
-		.wframe_data    (cmos_frame_Gray),
-		
-		.rframe_pclk    (w_pixel_clk            ),
-		//.rframe_vsync   (lcd_vs             ),		//	Reader VSync. Flush on rising edge. Connect to ~EOF. 
-		.rframe_vsync   (lcd_vs             ),		//	Reader VSync. Flush on rising edge. Connect to ~EOF.
-		.rframe_data_en (lcd_request             ),
-		.rframe_data    (lcd_data           ),
-		
-		.tp_o 		(w_axi_tp)
-	);
-	assign w_hbram_awid = 0; 
-	assign w_hbram_wid = 0; 
 	
-	
+	inter_connector #(
+        .TOP_DBW(16),          // 参数设置
+        .CSI_DATA_WIDTH(CSI_DATA_WIDTH)
+    ) u_inter_connector (
+        .w_sys_rst(w_sys_rst),                   // 输入信号连接
+        .sys_clk_i(sys_clk_i),
+        .hbramClk(hbramClk),
+        .hbramClk_Cal(hbramClk_Cal),
+        .hbramClk_shift(hbramClk_shift),         // 输出信号连接
+        .hbramClk_shift_sel(hbramClk_shift_sel),
+        .hbramClk_shift_ena(hbramClk_shift_ena),
+        .hbram_RST_N(hbram_RST_N),
+        .hbram_CS_N(hbram_CS_N),
+        .hbram_CK_P_HI(hbram_CK_P_HI),
+        .hbram_CK_P_LO(hbram_CK_P_LO),
+        .hbram_CK_N_HI(hbram_CK_N_HI),
+        .hbram_CK_N_LO(hbram_CK_N_LO),
+        .hbram_RWDS_OUT_HI(hbram_RWDS_OUT_HI),
+        .hbram_RWDS_OUT_LO(hbram_RWDS_OUT_LO),
+        .hbram_RWDS_IN_HI(hbram_RWDS_IN_HI),
+        .hbram_RWDS_IN_LO(hbram_RWDS_IN_LO),
+        .hbram_DQ_IN_LO(hbram_DQ_IN_LO),
+        .hbram_DQ_IN_HI(hbram_DQ_IN_HI),
+        .hbram_RWDS_OE(hbram_RWDS_OE),
+        .hbram_DQ_OUT_HI(hbram_DQ_OUT_HI),
+        .hbram_DQ_OUT_LO(hbram_DQ_OUT_LO),
+        .hbram_DQ_OE(hbram_DQ_OE),
+        .w_dev_rdata_i(w_dev_rdata_i),
+        .w_csi_rx_clk(w_csi_rx_clk),
+        .cmos_frame_vsync(cmos_frame_vsync),
+        .cmos_frame_href(cmos_frame_href),
+        .cmos_frame_Gray(cmos_frame_Gray),
+        .w_pixel_clk(w_pixel_clk),
+        .lcd_vs(lcd_vs),
+        .lcd_request(lcd_request),
+        .lcd_data(lcd_data)
+    );
 	
 	////////////////////////////////////////////////////////////////
 	//  LCD Timing Driver
